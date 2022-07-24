269e29e160ef62026ea64d4cff3787cdb96c93e4
Fedor Bystrov <augustbystrov@gmail.com>
2022-07-24 20:31:57 +0200

    clean up names

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 use std::fs;
 
-const PNG_DESCRIPTOR: [u8; 8] = [0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A];
+const PNG_HEADER: [u8; 8] = [0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A];
 
 fn main() {
     println!("Reading image file...");
 
     let read_result = fs::read("./images/house.png").expect("Can't read the image file");
-    let (descriptor, _right) = read_result.split_at(8);
+    let (header, _right) = read_result.split_at(8);
 
-    if descriptor != PNG_DESCRIPTOR {
+    if header != PNG_HEADER {
         panic!("Incorrect image format! png image expected");
     }
 
